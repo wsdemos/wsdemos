7f28954a08847136b453b63c9efcf3644e6068a3
wsdemos <179575262@qq.com>
2018-10-28 03:00:40 +0800

    Revert "Revert "Add files via upload""
    This reverts commit 85a7cdddc38c1e2dc8a2473a32baa0b9d60a704f.

---
+++ 我的博客h/js/new_file.js
@@ -0,0 +1,14 @@
+$('.slider_btn').click(function(){
+	showSlider();
+})
+$('.mask').click(function(){
+	hideSlider()
+})
+   function showSlider(){
+   	$('.mask').fadeIn()
+   	$('.silder').css('right',0)
+   }
+function hideSlider(){
+   	$('.mask').fadeOut()
+   	$('.silder').css('right',-300)
+}
